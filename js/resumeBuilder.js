583a9fb44d3381e210fa2533702badd9a2db97f3
WendyKin <jinwj2003@foxmail.com>
2018-05-07 14:00:33 +0800

    Modified Readme & css & add footcontacts to resumeBuilder.js.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -87,18 +87,23 @@
 
 		var formattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
 		$("#topContacts").append(formattedMobile);
+		$("#footerContacts").append(formattedMobile);
 
 		var formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
 		$("#topContacts").append(formattedEmail);
+		$("#footerContacts").append(formattedEmail);
 
 		var formattedTwitter = HTMLtwitter.replace("%data%",bio.contacts.twitter);
 		$("#topContacts").append(formattedTwitter);
+		$("#footerContacts").append(formattedTwitter);
 		
 		var formattedGithub = HTMLgithub.replace("%data%",bio.contacts.github);
 		$("#topContacts").append(formattedGithub);
+		$("#footerContacts").append(formattedGithub);
 
 		var formattedLocation = HTMLlocation.replace("%data%",bio.contacts.location);
 		$("#topContacts").append(formattedLocation);
+		$("#footerContacts").append(formattedLocation);
 
 		var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
 		$("#header").append(formattedWelcomeMsg);
